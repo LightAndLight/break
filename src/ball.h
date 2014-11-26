cf8756fe768daf9f2a4f6483d05acad2a416b069
Isaac Elliott <isaace71295@gmail.com>
2014-11-27 09:40:33 +1000

    Created ball class

---
+++ src/ball.h
@@ -0,0 +1,22 @@
+#ifndef BALL_H
+#define BALL_H
+
+#include<SFML/Graphics.hpp>
+
+class Ball {
+    sf::Sprite sprite;
+    sf::Vector2f direction;
+    int speed;
+
+    public:
+        Ball();
+        Ball(sf::Texture& tex,sf::IntRect rect);
+        sf::Vector2f getDir();
+        void reflectX();
+        void reflectY();
+        int getSpeed();
+        void setPosition(float x, float y);
+        void move(float x, float y);
+};
+
+#endif // BALL_H
