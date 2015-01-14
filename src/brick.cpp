020d16d7c3545fc35cd4d3de6f2806463c1de31f
Isaac Elliott <isaace71295@gmail.com>
2015-01-14 13:53:51 +1000

    Added color changing on hit to bricks

--- src/brick.cpp
+++ src/brick.cpp
@@ -5,7 +5,7 @@ Brick::Brick() {}
 Brick::Brick(sf::Texture& tex, sf::IntRect rec) 
 : Object(tex,rec) {
     visible = true;
-    hits = 2;
+    hits = 8;
 }
 
 bool Brick::isVisible() {
@@ -14,6 +14,32 @@ bool Brick::isVisible() {
 
 void Brick::recieveHit() {
     hits -= 1;
+
+    switch (hits) {
+        case 1:
+            setColor(127,0,255);
+            break;
+        case 2:
+            setColor(75,0,130);
+            break;
+        case 3:
+            setColor(0,0,255);
+            break;
+        case 4:
+            setColor(0,255,0);
+            break;
+        case 5:
+            setColor(255,255,0);
+            break;
+        case 6:
+            setColor(255,127,0);
+            break;
+        case 7:
+            setColor(255,0,0);
+            break;
+        default:
+            break;
+    }
 }
 
 int Brick::hitsRemaining() {
